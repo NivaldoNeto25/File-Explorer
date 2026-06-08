f213856db1d4b849a7b6534029871533e88ecaa3
NivaldoNeto25 <nivaldonetocontato@gmail.com>
2026-06-08 19:55:34 -0300

    Alterando o main para criar a pasta root

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 import sistema.Navegador;
 import sistema.OperadorDeDisco;
 import terminal.Terminal;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        // Cria a pasta no diretório do projeto
+        File pastaRoot = new File(System.getProperty("user.dir"), "root");
+        
+        if (!pastaRoot.exists()) {
+            pastaRoot.mkdir();
+        }
 
-        String caminhoInicial = System.getProperty("user.home");
-
-        Navegador navegador = new Navegador(caminhoInicial);
+        Navegador navegador = new Navegador(pastaRoot.getAbsolutePath());
         OperadorDeDisco operador = new OperadorDeDisco();
 
         Terminal terminal = new Terminal(navegador, operador);
